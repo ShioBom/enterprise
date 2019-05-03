478e07d4fde2f855a7aabb0add171ede8b94fda7
tangyu <15292061516@163.com>
2019-05-03 08:43:07 +0800

    完成了选座页面禁用座位的的显示

--- controller/movieController.js
+++ controller/movieController.js
@@ -74,8 +74,8 @@ module.exports = {
     };
   },
   //观影厅页面获取电影信息的
-   getCinemaMovie: function () {
-    return function (req, res, next) {
+  getCinemaMovie: function() {
+    return function(req, res, next) {
       let sql =
         "SELECT m.m_id,`m_name`,`m_type`,`m_length`FROM movie AS m INNER JOIN actor AS a ON m.m_id=a.m_id WHERE m.m_id=?";
       dbhelper.query(sql, req.body.id, (err, result) => {
@@ -89,9 +89,9 @@ module.exports = {
     };
   },
   //获取影厅信息
- getChambers: function () {
-    return function (req, res, next) {
-      let params =[req.body.m_id,req.body.c_id];
+  getChambers: function() {
+    return function(req, res, next) {
+      let params = [req.body.m_id, req.body.c_id];
       let sql =
         "SELECT `ch_time`,`ch_lang`,`name`,chamber.id FROM chamber_movie INNER JOIN chamber ON chamber_movie.id=chamber.id AND m_id=? AND c_id=?";
       dbhelper.query(sql, params, (err, result) => {
@@ -105,8 +105,8 @@ module.exports = {
     };
   },
   //根据影院id获取影院信息
-  getCinema: function () {
-    return function (req, res, next) {
+  getCinema: function() {
+    return function(req, res, next) {
       let params = req.body.c_id;
       let sql = "SELECT * FROM cinema WHERE c_id =?";
       dbhelper.query(sql, params, (err, result) => {
@@ -119,12 +119,42 @@ module.exports = {
       });
     };
   },
-  //向数据库中添加订单信息
-  insertOrder:function() {
+  //查询该影厅已经被购买的座位
+  seatIsBought: function () {
     return function (req, res, next) {
-      let params=[];
+      let sql =
+        "SELECT seat FROM orderform WHERE STATUS=0 AND o_time=? AND m_name=? AND cinema=? AND chamber=?;";
+      let params = [
+        req.body.time,
+        req.body.m_name,
+        req.body.cinema,
+        req.body.chamber
+      ];
+      console.log(params);
+      dbhelper.query(sql, params, (err, result) => {
+        console.log(result);
+        if (!err) {
+          res.json({
+            status: 1,
+            msg: "查询已被购买的座位成功",
+            result
+          });
+        } else {
+          res.json({
+            status: -1,
+            msg: "查询失败"
+          });
+          console.log(err);
+        }
+      });
+    };
+  },
+  //向数据库中添加订单信息
+  insertOrder: function() {
+    return function(req, res, next) {
+      let params = [];
       req.body.seats.forEach(ele => {
-        let arr=[
+        let arr = [
           req.body.u_id,
           req.body.m_name,
           req.body.time,
@@ -137,10 +167,10 @@ module.exports = {
         console.log(params);
       });
       let sql =
-        "INSERT INTO orderform(`u_id`,`m_name`,`time`,`chamber`,`cinema`,`seat`,`price`) VALUES ?";
+        "INSERT INTO orderform(`u_id`,`m_name`,`o_time`,`chamber`,`cinema`,`seat`,`price`) VALUES ?";
       dbhelper.query(sql, [params], (err, result) => {
         if (!err) {
-          res.json({ code: 1, msg:"订单建立成功" });
+          res.json({ code: 1, msg: "订单建立成功" });
         } else {
           res.json({ code: -1, msg: "数据获取失败" });
           console.log(err);
@@ -148,4 +178,91 @@ module.exports = {
       });
     };
   },
+  //根据用户查询还未结算的订单数据
+  queryOrderByUser: function() {
+    return function(req, res, next) {
+      let sql =
+        "SELECT * FROM orderform WHERE STATUS=1 AND u_id=? AND m_name=? AND cinema=? AND chamber=?;";
+      let params = [
+        req.body.u_id,
+        req.body.m_name,
+        req.body.cinema,
+        req.body.chamber
+      ];
+      dbhelper.query(sql, params, (err, result) => {
+        console.log(result);
+        if (!err) {
+          res.json({
+            status: 1,
+            msg: "用户未支付订单数据查询成功",
+            result
+          });
+        } else {
+          res.json({
+            status: -1,
+            msg: "查询失败"
+          });
+          console.log(err);
+        }
+      });
+    };
+  },
+  //取消订单
+  cancelOrderByUser: function() {
+    return function(req, res, next) {
+      let sql =
+        "DELETE FROM orderform WHERE STATUS=1 AND u_id=? AND m_name=? AND cinema=? AND chamber=?;";
+      let params = [
+        req.body.u_id,
+        req.body.m_name,
+        req.body.cinema,
+        req.body.chamber
+      ];
+      dbhelper.query(sql, params, (err, result) => {
+        console.log(result);
+        if (!err) {
+          res.json({
+            status: 1,
+            msg: "订单取消成功",
+          });
+        } else {
+          res.json({
+            status: -1,
+            msg: "查询失败"
+          });
+          console.log(err);
+        }
+      });
+    };
+  },
+  //订单结算
+  checkoutByUser: function() {
+    return function(req, res, next) {
+      let sql =
+        "UPDATE orderform SET STATUS=0 WHERE STATUS=1 AND u_id=? AND m_name=? AND cinema=? AND chamber=?;";
+      let params = [
+        req.body.u_id,
+        req.body.m_name,
+        req.body.cinema,
+        req.body.chamber
+      ];
+      dbhelper.query(sql, params, (err, result) => {
+        console.log(result);
+        if (!err) {
+          res.json({
+            status: 1,
+            msg: "用户未支付订单数据查询成功",
+            
+          });
+        } else {
+          res.json({
+            status: -1,
+            msg: "查询失败"
+          });
+          console.log(err);
+        }
+      });
+    };
+  },
+  //根据用户查询所有的订单数据
 };
--- route/client/clientRouter.js
+++ route/client/clientRouter.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from "express";
 import movieController from "../../controller/movieController";
 let clientRouter = express.Router();
 module.exports = clientRouter;
@@ -11,4 +11,8 @@ clientRouter.post("/getCinemaMovie", movieController.getCinemaMovie());
 clientRouter.post("/getChambers", movieController.getChambers());
 clientRouter.post("/getCinema", movieController.getCinema());
 clientRouter.post("/insertOrder", movieController.insertOrder());
+clientRouter.post("/queryOrderByUser", movieController.queryOrderByUser());
+clientRouter.post("/seatIsBought", movieController.seatIsBought());
+clientRouter.post("/cancelOrderByUser", movieController.cancelOrderByUser());
+clientRouter.post("/checkoutByUser", movieController.checkoutByUser());
 
